a4476a97345f7b16ae0843fec6eeb18dd35cdec0
EinePriseCode <joern@vaupel-web.de>
2023-01-26 19:07:50 +0100

    Test commit

--- objects.py
+++ objects.py
@@ -15,7 +15,7 @@ class Sphere(Transform):
         self.radius = radius
         self.color = color
 
-    def hit_sphere(self, ray, t_min=1, t_max=1000):
+    def hit(self, ray, t_min, t_max):
         pointer = ray.origin - self.position
 
         # less efficient
@@ -82,13 +82,8 @@ class Camera(Transform):
                    + self.horizontal * (x + rand_offset_x) / (self.image_width - 1)
                    + self.vertical * (y + rand_offset_y) / (self.image_height - 1) - self.position)
 
-    def ray_color(self, ray, objects):
-        result = None
-        for obj in objects:
-            result_obj = obj.hit_sphere(ray, self.t_min, self.t_max)
-            if result_obj is not None:
-                if result is None or result_obj[0] < result[0]:
-                    result = result_obj
+    def ray_color(self, ray, scene):
+        result = scene.hit(ray, self.t_min, self.t_max)
 
         if result is not None:
             _, pos, n, color = result
@@ -100,7 +95,7 @@ class Camera(Transform):
         t = .5 * (unit_dir.y + 1)
         return Vector(255, 255, 255) * (1-t) + Vector(np.floor(255 * .5), np.floor(255 * .7), 255) * t
 
-    def render(self, objects):
+    def render(self, scene):
         i = Image(self.image_width, self.image_height)
         # looping through pixels for rendering
         for y in range(self.image_height)[::-1]:
@@ -108,7 +103,7 @@ class Camera(Transform):
                 pixel_color = Vector(0, 0, 0)
                 for s in range(self.samples_per_pixel):
                     ray = self.get_ray(x, y, self.samples_per_pixel > 1)
-                    pixel_color = pixel_color + self.ray_color(ray, objects)
+                    pixel_color = pixel_color + self.ray_color(ray, scene)
                 i.image_list[y, x] = self.average_color(pixel_color).to_int_array()
 
         return i
@@ -117,3 +112,63 @@ class Camera(Transform):
         # doesn't have to be clamped because no single summed up color value is bigger then 255
         # floor division to avoid float colors
         return pixel_color // self.samples_per_pixel
+
+
+class Scene:
+    def __init__(self, name, path=""):
+        self.name = name
+        self.path = path
+        self.objects = []
+        self.cameras = []
+
+    def render(self):
+        for i in range(len(self.cameras)):
+            img = self.cameras[i].render(self)
+            if i > 0:
+                img.save_image(f"{self.path}{self.name}-{i + 1}.ppm")
+            else:
+                img.save_image(f"{self.path}{self.name}.ppm")
+
+    def add_cam(self, cam):
+        self.cameras.append(cam)
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+    def hit(self, ray, t_min, t_max):
+        result = None
+        for obj in self.objects:
+            result_obj = obj.hit(ray, t_min, t_max)
+            if result_obj is not None:
+                if result is None or result_obj[0] < result[0]:
+                    result = result_obj
+
+
+scene = Scene("rendering5")
+main_camera = Camera(1, 16 / 9, 100)
+cam2 = Camera(1, 16 / 9, 100, samples_per_pixel=2)
+cam3 = Camera(1, 16 / 9, 100, samples_per_pixel=4)
+cam4 = Camera(1, 16 / 9, 100, samples_per_pixel=8)
+cam5 = Camera(1, 16 / 9, 100, samples_per_pixel=16)
+
+scene.add_cam(main_camera)
+scene.add_cam(cam2)
+scene.add_cam(cam3)
+scene.add_cam(cam4)
+scene.add_cam(cam5)
+
+s0 = Sphere(Vector(0, 0, -3), 1, Vector(123, 213, 132))
+s1 = Sphere(Vector(3, 2, -7), .5, Vector(0, 0, 255))
+s2 = Sphere(Vector(-1, 0, -10), 3, Vector(0, 0, 0))
+s3 = Sphere(Vector(-2, 1, -2), .2, Vector(255, 0, 0))
+s4 = Sphere(Vector(0, -1, -2), 1, Vector(0, 255, 0))
+
+scene.add_object(s0)
+scene.add_object(s1)
+scene.add_object(s2)
+scene.add_object(s3)
+scene.add_object(s4)
+
+scene.render()
+
+
